fbc1e786065fc11859be594c891a25b3b58a80af
Matheus Severo <matheus.severo02@gmail.com>
2025-05-28 18:08:54 -0300

    Revert "Teste"
    This reverts commit 386362d62a10b4966aaec52176f5761bb960b20c.

--- Program.cs
+++ Program.cs
@@ -1,37 +1,25 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddControllers();
-
-// Registrar serviÃ§os customizados
-builder.Services.AddScoped<SeveroTechLanding.Services.IEmailService, SeveroTechLanding.Services.EmailService>();
-
-// Configurar CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
-app.UseCors("AllowAll");
+
 app.UseAuthorization();
 
 app.MapRazorPages();
-app.MapControllers();
 
 app.Run();
